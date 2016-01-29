283a341bc64013c939a46fd265441774473fd1ed
Zhexxxian <zhangzhexian@outlook.com>
2016-01-29 12:08:22 +0800

    Basic UI -- layout, circular buttons, colour scheme, functional button position

--- app/src/main/java/com/example/zhexian/souschef10/MainActivity.java
+++ app/src/main/java/com/example/zhexian/souschef10/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.zhexian.souschef10;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,17 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
